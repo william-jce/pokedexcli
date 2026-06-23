7056d242297e1831ad987ea8762209bb9bfc420f
william-jce <william.entschluss@gmail.com>
2026-06-23 19:45:44 +0900

    Refactor cleanInput

--- repl.go
+++ repl.go
@@ -5,11 +5,7 @@ import (
 )
 
 func cleanInput(text string) []string {
-	var result []string
-
-	for word := range strings.FieldsSeq(text) {
-		result = append(result, strings.ToLower(word))
-	}
-
+	words := strings.ToLower(text)
+	result := strings.Fields(words)
 	return result
 }
